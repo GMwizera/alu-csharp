be2299b90354962fe13b07c94db86f78218895e9
giseleMwizera <mamengisele@gmail.com>
2026-05-17 22:44:01 +0200

    Concat

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "Holberton School";
+        Console.WriteLine(str + str + str);
+        Console.WriteLine(str.Substring(0, 9));
+    }
+}
