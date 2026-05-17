c111ec2fa59396a29209861453d5a0c55fb6cd71
giseleMwizera <mamengisele@gmail.com>
2026-05-17 22:40:45 +0200

    Concat

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str1 = "Holberton";
+		string str2 = "School";
+		str1 = str1 + " " + str2;
+		Console.WriteLine("Welcome to {0}!", str1);
+	}
+}
