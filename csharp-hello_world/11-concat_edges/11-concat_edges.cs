b7cfc74bf2a358b56d358a082054f2661bc9c40a
giseleMwizera <mamengisele@gmail.com>
2026-05-17 23:03:18 +0200

    Add

---
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "<full template string here>";
+        str = str.Substring(A, B) + str.Substring(C, D) + str.Substring(E, F) + str.Substring(G, H);
+        Console.WriteLine(str);
+    }
+}
