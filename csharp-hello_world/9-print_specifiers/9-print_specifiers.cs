2ddd508076a34ae0c1881a049d48930de44ec36c
giseleMwizera <mamengisele@gmail.com>
2026-05-17 22:53:10 +0200

    Print specifiers

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double percent = .7553;
+        double currency = 98765.4321;
+        CultureInfo culture = CultureInfo.GetCultureInfo("en-US");
+        Console.WriteLine("Percent: " + percent.ToString("P2", culture));
+        Console.WriteLine("Currency: " + currency.ToString("C2", culture));
+    }
+}
