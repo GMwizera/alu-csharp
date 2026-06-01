f964c05014d6712832ca850158a8a8d6610b0a54
giseleMwizera <mamengisele@gmail.com>
2026-06-01 10:31:24 +0200

    Reverse array question

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Array
+{
+    public static int[] Reverse(int[] array)
+    {
+        if (array == null)
+        {
+            Console.WriteLine("$");
+            return array;
+        }
+        if (array.Length == 0)
+        {
+            Console.WriteLine("$");
+            return array;
+        }
+        for (int i = 0; i < array.Length / 2; i++)
+        {
+            int temp = array[i];
+            array[i] = array[array.Length - i - 1];
+            array[array.Length - i - 1] = temp;
+        }
+        Console.WriteLine(string.Join(" ", array) + "$");
+        return array;
+    }
+}
