80fd9caddfaea5f4d10bd1e71bbfa183790de2c9
giseleMwizera <mamengisele@gmail.com>
2026-06-03 11:31:23 +0200

    Added Number 12

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+	
+	public static void PrintSorted(Dictionary<string, string> myDict)
+	{
+		var keys = new List<string>();
+		foreach (var entry in myDict)
+			keys.Add(entry.Key);
+		keys.Sort(StringComparer.Ordinal);
+		foreach (var key in keys)
+			Console.WriteLine("{0}: {1}", key, myDict[key]);
+	}
+}
