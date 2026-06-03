89d6d230c6d0f6427e8f20a8f084db451cc0dac3
giseleMwizera <mamengisele@gmail.com>
2026-06-03 10:44:04 +0200

    Added 9

---
+++ csharp-arrays_lists_dictionaries/9-add_key_value/9-main.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        myDict.Add(key, value);
+    }
+}
