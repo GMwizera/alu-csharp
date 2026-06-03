7cb1008a38f8538e87f144d5b867d068109630bf
giseleMwizera <mamengisele@gmail.com>
2026-06-03 09:33:02 +0200

    4. Print List

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int> list = new List<int>();
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return list;
+        }
+        for (int i = 0; i < size; i++)
+            list.Add(i);
+        Console.WriteLine(string.Join(" ", list));
+        return list;
+    }
+}
--- csharp-hello_world/3-writeline/obj/Debug/net7.0/3-writeline.AssemblyInfo.cs
+++ csharp-hello_world/3-writeline/obj/Debug/net7.0/3-writeline.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3-writeline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed9537d15428bcb965b540fb2f2440380c01746a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0410b7e731bf7ecdb1127df9280a2199b977e80")]
 [assembly: System.Reflection.AssemblyProductAttribute("3-writeline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3-writeline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
