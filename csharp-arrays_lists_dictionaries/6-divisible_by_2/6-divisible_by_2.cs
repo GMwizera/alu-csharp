847fcaec9ed68b0b4612f0fcb81dfef66e31b2a3
giseleMwizera <mamengisele@gmail.com>
2026-06-03 09:49:36 +0200

    [Add] 6-Divisible By 2

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class List
+{
+   public static List<bool> DivisibleBy2(List<int> myList)
+   {
+    List<bool> divisible = new List<bool>();
+    for (int i = 0; i < myList.Count; i++)
+        divisible.Add(myList[i] % 2 == 0);
+    return divisible;
+    }
+}
