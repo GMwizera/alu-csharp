70de211e0f6315e696c56ef5cacf5f81899c7237
giseleMwizera <mamengisele@gmail.com>
2026-06-03 10:24:47 +0200

    [ADD] 8-Number Keys

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int total = 0;
+        foreach (var key in myDict.Keys)
+            total++;
+        return total;
+    }
+}
