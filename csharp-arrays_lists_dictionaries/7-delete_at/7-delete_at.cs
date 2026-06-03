2d25e572f4917630241131b993652b469985e014
giseleMwizera <mamengisele@gmail.com>
2026-06-03 10:00:54 +0200

    Fix: Abolished the use of RemoveAt function

--- csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -1,15 +1,21 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 class List
 {
-   public static List<int> DeleteAt(List<int> myList, int index)
-   {
-    if (index < 0 || index >= myList.Count)
+    public static List<int> DeleteAt(List<int> myList, int index)
     {
-        Console.WriteLine("Index is out of range");
-        return myList;
-    }
-    myList.RemoveAt(index);
-    return myList;
+        if (index < 0 || index >= myList.Count)
+        {
+            Console.WriteLine("Index is out of range");
+            return myList;
+        }
+        List<int> newList = new List<int>();
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (i != index)
+                newList.Add(myList[i]);
+        }
+        return newList;
     }
 }
